c9f125e47bb67476f6594fc137bd870b73b12fbd
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2022-02-22 15:23:02 -0600

    update: scripts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
-import dynamoose from 'dynamoose';
-import dotenv from 'dotenv';
-import Person from './Person';
-
-dynamoose.aws.sdk.config.update({ region: 'us-east-1' });
 
 const app = express();
 
 app.use(json());
+
+app.get('/health', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify({
+    message: 'Target group is healthy'
+  }));
+});
 
 app.get('/hello', (req, res) => {
   res.writeHead(200, {
@@ -17,19 +21,6 @@ app.get('/hello', (req, res) => {
   res.end(JSON.stringify({
     message: 'Hello world!'
   }));
-});
-
-app.post('/people', async (req, res) => {
-  const { id, name, age } = req.body;
-
-  const person = new Person({ id, name, age });
-  await person.save();
-
-  res
-    .writeHead(200, {
-      'Content-Type': 'application/json'
-    })
-    .end();
 });
 
 app.listen(3000, () => {
